1332b504bd147dd91984510a3d21f1e21248bb42
dharmawipraja <v-imade.wipraja@btpn.com>
2020-05-18 02:09:32 +0700

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
